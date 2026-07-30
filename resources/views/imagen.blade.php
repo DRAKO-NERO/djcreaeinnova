3e3b257ac4daa7ede56d1df57b6aecc319b7f12a
DRAKO-NERO <osadsuza@gmail.com>
2026-07-30 07:20:47 -0500

    Restaurar titulos rojos y mantener maqueta responsiva en vistas

--- resources/views/imagen.blade.php
+++ resources/views/imagen.blade.php
@@ -12,7 +12,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>DJcreaeinnova - muebles personalizados</title>
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
-
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -24,20 +23,20 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-    </div>
-      <a href="{{url('/')}}" class="btn btn-primary">Inicio</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent"></div>
+    <a href="{{url('/')}}" class="btn btn-primary">Inicio</a>
   </div>
 </nav>
 
 <br>
-<div class="container p-4" style="background-color: #00000083; border-radius: 10px; box-shadow: 0px 0px 10px #000000;">
-    <center>
-        <h3 class="titulo1 text-white">DJ CREA E INNOVA</h3>
-        <br>
-    </center>
-  <div class="container text-center my-3">
+<!-- Título principal con estilo original -->
+<center>
+    <h3 class="titulo1">DJ CREA E INNOVA</h3>
+</center>
+<br>
+
+<div class="container" style="background-color: #00000083; border-radius: 10px; box-shadow: 0px 0px 10px #000000; padding: 20px;">
+  <div class="container text-center my-2">
     <div class="row mx-auto my-auto justify-content-center">
 
     <!-- Main content -->
@@ -46,59 +45,54 @@
         <div class="row">
 
           <div class="col-md-12">
-
-            <div class="card card-primary card-outline shadow">
-              <div class="card-header bg-light">
+            <div class="card card-primary card-outline">
+              <div class="card-header bg-white">
                 <h3 class="m-0 text-dark fw-bold">{{$imagen->titulo_i}}</h3>
               </div>
               <div class="card-body p-4 text-start">
                 
-                <!-- SISTEMA DE COLUMNAS PARA IMAGEN Y DESCRIPCIÓN -->
-                <div class="row align-items-center">
+                <div class="row align-items-start">
                     
                     <!-- Columna de la Imagen -->
                     <div class="col-12 col-md-5 col-lg-5 text-center mb-4 mb-md-0">
-                        <h5 class="fw-bold mb-3 text-uppercase">Imagen</h5>
+                        <h5 class="fw-bold mb-3 text-uppercase text-primary">IMAGEN</h5>
                         <div class="shadow-sm rounded overflow-hidden mx-auto" style="max-width: 500px;">
-                            <img src="{{asset('storage').'/'.$imagen->imagen_i}}" class="img-fluid rounded w-100" style="max-height: 400px; object-fit: cover;" alt="{{$imagen->titulo_i}}">
+                            <img src="{{asset('storage').'/'.$imagen->imagen_i}}" class="img-fluid rounded w-100" style="max-height: 420px; object-fit: cover;" alt="{{$imagen->titulo_i}}">
                         </div>
                     </div>
 
                     <!-- Columna de la Descripción -->
                     <div class="col-12 col-md-7 col-lg-7">
-                        <h5 class="fw-bold mb-3 text-uppercase text-center text-md-start">Descripción</h5>
+                        <h5 class="fw-bold mb-3 text-uppercase text-primary text-center text-md-start">DESCRIPCIÓN</h5>
                         <div class="descripcion-contenido p-2" style="font-size: 1.05rem; line-height: 1.7;">
                             {!! $imagen->descripcion_i !!}
                         </div>
                     </div>
 
                 </div>
-                <!-- FIN SISTEMA DE COLUMNAS -->
 
               </div>
             </div>
           </div>
-          <!-- /.col-md-12 -->
+
         </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
+      </div>
     </div>
-    <!-- /.content -->
 
     </div>
   </div>
 </div>
 <br><br>
-<!-- Botones de Redes Sociales con clase estándar text-center -->
-    <div class="text-center my-4">
-        <a href="https://www.facebook.com/profile.php?id=61568396361484" target="_blank" rel="noopener noreferrer" class="btn btn-primary btn-lg btn-facebook animate__animated animate__zoomInUp animate__delay-1s"><i class="bi bi-facebook"></i></a>
-        <a href="https://youtube.com/@djcreaeinnova?si=hmKOT50TGAqDILxV" target="_blank" rel="noopener noreferrer" class="btn btn-danger btn-lg btn-youtube animate__animated animate__zoomInUp animate__delay-2s"><i class="bi bi-youtube"></i></a>
-        <a href="https://www.tiktok.com/@mister.mueble.per?_r=1&_t=ZS-98RzkitaoIS" target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-tiktok animate__animated animate__zoomInUp animate__delay-3s"><i class="bi bi-tiktok"></i></a>
-        <a href="https://www.instagram.com/djcreaeinnova?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-instagram animate__animated animate__zoomInUp animate__delay-4s"><i class="bi bi-instagram"></i></a>
-        <a href="https://wa.me/51915061691" target="_blank" rel="noopener noreferrer" class="btn btn-secondary btn-lg btn-whatsapp animate__animated animate__zoomInUp animate__delay-5s"><i class="bi bi-whatsapp"></i></a>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/carrousel.js') }}"></script>
+<center>
+    <a href="https://www.facebook.com/profile.php?id=61568396361484" target="_blank" class="btn btn-primary btn-lg btn-facebook animate__animated animate__zoomInUp animate__delay-1s"><i class="bi bi-facebook"></i></a>
+    <a href="https://youtube.com/@djcreaeinnova?si=hmKOT50TGAqDILxV" target="_blank" class="btn btn-danger btn-lg btn-youtube animate__animated animate__zoomInUp animate__delay-2s"><i class="bi bi-youtube"></i></a>
+    <a href="https://www.tiktok.com/@mister.mueble.per?_r=1&_t=ZS-98RzkitaoIS" target="_blank" class="btn btn-lg btn-tiktok animate__animated animate__zoomInUp animate__delay-3s"><i class="bi bi-tiktok"></i></a>
+    <a href="https://www.instagram.com/djcreaeinnova?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" class="btn btn-lg btn-instagram animate__animated animate__zoomInUp animate__delay-4s"><i class="bi bi-instagram"></i></a>
+    <a href="https://wa.me/51915061691" target="_blank" class="btn btn-secondary btn-lg btn-whatsapp animate__animated animate__zoomInUp animate__delay-5s"><i class="bi bi-whatsapp"></i></a>
+</center>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="{{ asset('js/carrousel.js') }}"></script>
 </body>
 </html>
